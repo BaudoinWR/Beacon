4c59eabc923eef9ca7b43ae89119416bb5016162
Baudoin Woods Ribadeau <baudoin.wr@gmail.com>
2017-09-13 17:29:52 +0200

    Refactoring in the WebService.

--- beacon-web/src/main/java/fr/woorib/beacon/web/BeaconRest.java
+++ beacon-web/src/main/java/fr/woorib/beacon/web/BeaconRest.java
@@ -3,37 +3,36 @@ package fr.woorib.beacon.web;
 import java.math.BigDecimal;
 import java.util.List;
 import javax.inject.Inject;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import fr.woorib.beacon.data.BeaconEntry;
 import fr.woorib.beacon.services.BeaconService;
 
 /**
  * Created by Veryeld on 02/04/2017.
  */
-@Controller
+@RestController
+@RequestMapping("/beacon")
 public class BeaconRest {
 
     @Inject
     private BeaconService beaconService;
 
-  @RequestMapping("beaconForUser")
-  @ResponseBody
-  public List<BeaconEntry> getBeaconForUser(@RequestParam(value="userId", required=true) Integer userId, Model model) {
-    return beaconService.getUserBeacons(userId);
+  @GetMapping("/user/{id}")
+  public List<BeaconEntry> getBeaconForUser(@PathVariable(required=true) Integer id, Model model) {
+    return beaconService.getUserBeacons(id);
   }
 
-  @RequestMapping("beaconById")
-  @ResponseBody
-  public BeaconEntry getBeaconById(@RequestParam(value="beaconId", required=true) Integer beaconId, Model model) {
-    return beaconService.getBeacon(beaconId);
+  @GetMapping(path = "/{id}")
+  public BeaconEntry getBeaconById(@PathVariable(required=true) Integer id, Model model) {
+    return beaconService.getBeacon(id);
   }
 
-  @RequestMapping("createBeacon")
-  @ResponseBody
+  @GetMapping("createBeacon")
   public BeaconEntry createBeacon(@RequestParam(value="userId", required=true) Integer userId,
                                   @RequestParam(value="longitude", required=true) BigDecimal longitude,
                                   @RequestParam(value="latitude", required=true) BigDecimal latitude,
